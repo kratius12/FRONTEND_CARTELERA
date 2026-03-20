1b638bc48822eb10897d6987f957af015279d194
Ronald <ortizronald623@gmail.com>
2026-03-19 19:27:49 -0500

    feat: Update Vite proxy target to the production backend URL.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:3001',
+        target: 'https://backendcartelera-production-a48a.up.railway.app/',
         changeOrigin: true,
       }
     }
